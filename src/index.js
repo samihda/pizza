6cef4bf64655b15478f934e13602e2d0a4480599
samihda <samihda@users.noreply.github.com>
2016-07-22 00:41:40 +0200

    css for background

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import Header from './containers/Header';
+import './styles/main.css';
 
 const store = configureStore();
 
