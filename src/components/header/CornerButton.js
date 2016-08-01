257df26fb26fe58edf287ec7e62a6234552f00ed
samihda <samihda@users.noreply.github.com>
2016-08-01 14:19:05 +0200

    style classes

--- src/components/header/CornerButton.js
+++ src/components/header/CornerButton.js
@@ -3,11 +3,16 @@ import React from 'react';
 const CornerButton = ({ step, onStartClick, onCancelClick }) => {
   switch (step) {
     case 0:
-      return (<button onClick={onStartClick}>{`ORDER TODAY'S MEAL`}</button>);
+      return (<button className="red pure-button button-large" onClick={onStartClick}>ORDER TODAY'S MEAL</button>);
     case 4:
-      return (<button>{`ORDER SENT`}</button>);
+      return (<button className="green pure-button button-large">ORDER SENT</button>);
     default:
-      return (<button onClick={onCancelClick}>{`CANCEL ORDER`}</button>);
+      return (
+        <button className="red pure-button button-large icon-button" onClick={onCancelClick}>
+          <img className="icon-left" src="/assets/icons/cancel_icon.svg" />
+          <span>CANCEL ORDER</span>
+        </button>
+      );
   }
 };
 
