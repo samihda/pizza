831d04fe3da6c16528736152547856887b2b1138
samihda <samihda@users.noreply.github.com>
2016-08-01 19:05:57 +0200

    splash component styles

--- src/components/main/Splash.js
+++ src/components/main/Splash.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 const Splash = ({ onStartClick }) => (
   <main>
-    <img src="/assets/logo/feedme_logo_white.svg" />
-    <h1>Chosen food every 5 days</h1>
-    <p>Today on the table: <span>Original Steinofen-Pizza</span></p>
-    <button onClick={onStartClick}>
-      <img src="/assets/icons/order_icon.svg" />
-      Order Today’s Meal
-    </button>
-    <p>Check out our meal for tomorrow.</p>
-    <small>Made with love.</small>
-    <img src="/assets/icons/scroll_icon.svg" />
+    <div>
+      <img src="/assets/logo/feedme_logo_white.svg" />
+      <h1>Chosen food every 5 days</h1>
+      <p>Today on the table: <span className="bottom-shadow-red">Original Steinofen-Pizza</span></p>
+    </div>
+    <div>
+      <button onClick={onStartClick} id="splash-button" className="pure-button">
+        <img className="icon-left" src="/assets/icons/order_icon.svg" />
+        <span>Order Today’s Meal</span>
+      </button>
+    </div>
+    <div>
+      <p>Check out our meal for tomorrow.</p>
+      <small>Made with love.</small>
+      <div><img src="/assets/icons/scroll_icon.svg" /></div>
+    </div>
   </main>
 );
 
